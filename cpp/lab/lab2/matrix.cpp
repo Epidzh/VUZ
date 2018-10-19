44486f326b603b6aa3598b660e8317af0e0b412f
e1nfalt <sergio.petrovx@yandex.ru>
2018-10-19 23:02:30 +0300

    lab2 update

--- cpp/lab/lab2/matrix.cpp
+++ cpp/lab/lab2/matrix.cpp
@@ -1,96 +1,92 @@
-#include <iostream>
-#include <string>
+//
+//  matrix.h
+//  cpp_solutions
+//
+//  Created by e1nfalt on 18/10/2018.
+//  Copyright © 2018 SergioPetrovx. All rights reserved.
+//
+
 #include "matrix.h"
 
-void passed() {
-    static int n=0;
-    std::cout << "Test " << ++n << " passed" << std::endl;
+Matrix Matrix::T()
+{
+    
+    Matrix M(rows, cols);
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            M.elems[i][j] = elems[j][i];
+    return M;
 }
 
-void testMatrix() {
+Matrix Matrix::operator*(Matrix M2)
+{
+    if (rows != M2.cols)
+        throw MatrixExeption((char*)"Can't multiply matrices with dimensions A[n][q] * B[p][m], where q!=p");
     
-    Matrix a(2, 3), b(2, 3), c(3, 3);
+    Matrix M(cols, M2.rows);
     
-    c = 2 * c; //Multiply all elements by number.
-    
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 2; j++) {
-            a[j][i] = i + j + i*j * 2;
-            b[j][i] = i - j * 7;
+    for (int i = 0; i < cols; i++)
+        for (int j = 0; j < M2.rows; j++)
+        {
+            M.elems[i][j] = 0;
+            for (int k = 0; k < M2.cols; k++)
+                M.elems[i][j] += elems[i][k] * M2.elems[k][j];
         }
-    }
-
-    try {
-        c = c * (a*b);
-        std::cout << "Error. (a*b) should throw MatrixExeption exception." << std::endl;
-    }
-    catch (MatrixExeption e) {
-        if (e.what() != std::string("Can't multiply matrices with dimensions A[n][q] * B[p][m], where q!=p")) {
-            std::cout << "Wrong exeption message" << std::endl;
-        }
-        else {
-            passed();
-        }
-    }
-    
-    a = a.T(); //transposition
-    c = c * (a*b).T();
-    
-    int sum = 0;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            sum += c[i][j]*(i+1)*(j+1);
-        }
-    }
-    
-    if (sum != -1912) {
-        std::cout << "Error in calculating c = c * (a*b).T();" << std::endl;
-    }
-    else {
-        passed();
-    }
-    
-    if (c != c.T().T()) {
-        std::cout << "A twice-transposed matrix is not equal to the matrix itself." << std::endl;
-        return;
-    }
-    else {
-        passed();
-    }
-    Matrix d(2, 2);
-    
-    try {
-        d = c + d;
-        std::cout << "Error. c + d should throw MatrixExeption exception." << std::endl;
-    }
-    catch (MatrixExeption e) {
-        if (e.what() != std::string("Can't sum matrices with different dimensions")) {
-            std::cout << "Wrong exeption message" << std::endl;
-        }
-        else {
-            passed();
-        }
-    }
-    
-    try {
-        d = c - d;
-        std::cout << "Error. c - d should throw MatrixExeption exception." << std::endl;
-    }
-    catch (MatrixExeption e) {
-        if (e.what() != std::string("Can't subtract matrices with different dimensions")) {
-            std::cout << "Wrong exeption message" << std::endl;
-        }
-        else {
-            passed();
-        }
-    }
+    return M;
 }
 
-int main() {
+Matrix operator*(int left, Matrix right)
+{
+
+    Matrix M(right.rows, right.cols);
+    for (int i = 0; i < right.cols; i++)
+    {
+
+        for (int j = 0; j < right.rows; j++)
+            M.elems[i][j] = left * right.elems[j][i];
+    }
+    return M;
+}
+
+int* Matrix::operator[](int index)
+{
+    return elems[index];
+}
+
+Matrix Matrix::operator+(Matrix M2)
+{
+    if ((rows == M2.rows) && (cols == M2.cols))
+    {
+        Matrix M(cols, M2.rows);
+        for (int i = 0; i < cols; i++)
+            for (int j = 0; j < rows; j++)
+                M.elems[i][j] = elems[i][j] + M2.elems[i][j];
+        return M;
+    }
+    else throw MatrixExeption((char*)"Can't sum matrices with different dimensions");
+}
+
+Matrix Matrix::operator-(Matrix M2)
+{
+    if ((rows == M2.rows) && (cols == M2.cols))
+    {
+        Matrix M(cols, M2.rows);
+        for (int i = 0; i < cols; i++)
+            for (int j = 0; j < rows; j++)
+                M.elems[i][j] = elems[i][j] - M2.elems[i][j];
+        return M;
+    }
+    else throw MatrixExeption((char*)"Can't subtract matrices with different dimensions");
+}
+
+bool Matrix::operator!=(Matrix M2)
+{
+    if (cols != M2.cols || rows != M2.rows)
+        return true;
     
-    testMatrix();
-    
-    std::cin.get();
-    
-    return 0;
+    for (int i = 0; i < cols; i++)
+        for (int j = 0; j < rows; j++)
+            if (elems[i][j] != M2.elems[i][j])
+                return true;
+    return false;
 }
--- cpp/lab/lab2/matrix.h
+++ cpp/lab/lab2/matrix.h
@@ -6,18 +6,18 @@
 //  Copyright © 2018 SergioPetrovx. All rights reserved.
 //
 
-#ifndef matrix_h
-#define matrix_h
+#ifndef _MATRIX_H_
+#define _MATRIX_H_
 
 class MatrixExeption
 {
 private:
-    std::string message;
+    char* message;
 public:
     
-    MatrixExeption(std::string str) : message(str) {};
+    MatrixExeption(char* str) : message(str) {};
     
-    std::string what()
+    char* what()
     {
         return message;
     }
@@ -25,11 +25,12 @@ public:
 
 class Matrix
 {
-public:
+private:
     
     int cols, rows;
     int** elems;
     
+public:
     Matrix(int cols_num, int rows_num)
     {
         
@@ -44,89 +45,13 @@ public:
         }
     }
     
-    Matrix T()
-    {
-        
-        Matrix M(rows, cols);
-        for (int i = 0; i < rows; i++)
-            for (int j = 0; j < cols; j++)
-                M.elems[i][j] = elems[j][i];
-        return M;
-    }
-    
-    friend Matrix operator*(Matrix M1, Matrix M2)
-    {
-        if (M1.rows != M2.cols)
-            throw MatrixExeption("Can't multiply matrices with dimensions A[n][q] * B[p][m], where q!=p");
-        
-        Matrix M(M1.cols, M2.rows);
-        
-        for (int i = 0; i < M1.cols; i++)
-            for (int j = 0; j < M2.rows; j++)
-            {
-                M.elems[i][j] = 0;
-                for (int k = 0; k < M2.cols; k++)
-                    M.elems[i][j] += M1.elems[i][k] * M2.elems[k][j];
-            }
-        return M;
-    }
-    
-    friend Matrix operator*(int left, Matrix right)
-    {
-        
-        Matrix M(right.rows, right.cols);
-        for (int i = 0; i < right.cols; i++)
-        {
-            
-            for (int j = 0; j < right.rows; j++)
-                M.elems[i][j] = left * right.elems[j][i];
-        }
-        return M;
-    }
-    
-    int* operator[](int index)
-    {
-        return elems[index];
-    }
-    
-    friend Matrix operator+(Matrix M1, Matrix M2)
-    {
-        if ((M1.rows == M2.rows) && (M1.cols == M2.cols))
-        {
-            Matrix M(M1.cols, M2.rows);
-            for (int i = 0; i < M1.cols; i++)
-                for (int j = 0; j < M1.rows; j++)
-                    M.elems[i][j] = M1.elems[i][j] + M2.elems[i][j];
-            return M;
-        }
-        else throw MatrixExeption("Can't sum matrices with different dimensions");
-    }
-    
-    friend Matrix operator-(Matrix M1, Matrix M2)
-    {
-        if ((M1.rows == M2.rows) && (M1.cols == M2.cols))
-        {
-            Matrix M(M1.cols, M2.rows);
-            for (int i = 0; i < M1.cols; i++)
-                for (int j = 0; j < M1.rows; j++)
-                    M.elems[i][j] = M1.elems[i][j] - M2.elems[i][j];
-            return M;
-        }
-        else throw MatrixExeption("Can't subtract matrices with different dimensions");
-    }
-    
-    friend bool operator!=(Matrix M1, Matrix M2)
-    {
-        if (M1.cols != M2.cols || M1.rows != M2.rows)
-            return true;
-        
-        for (int i = 0; i < M1.cols; i++)
-            for (int j = 0; j < M1.rows; j++)
-                if (M1.elems[i][j] != M2.elems[i][j])
-                    return true;
-        return false;
-    }
-    
+    Matrix T();
+    Matrix operator*(Matrix);
+    friend Matrix operator*(int, Matrix);
+    Matrix operator+(Matrix);
+    Matrix operator-(Matrix);
+    bool operator!=(Matrix);
+    int* operator[](int);
 };
 
-#endif /* matrix_h */
+#endif
