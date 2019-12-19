b4af6aacf970315b17f65062de138b9a2d491d76
e1nfalt <sergio.petrovx@yandex.ru>
2019-12-19 17:19:48 +0300

    delete os_FileBackup

--- os_FileBackup/test.sh
+++
@@ -1,9 +0,0 @@
-IFS=$'\n'
-files="$(find . -type f \( -perm -o+w \) -or \( -perm -g+w \) )"
-all_files="$(find . -type f)"
-echo "$all_files"
-for i in $files
-do
-    echo "$(realpath $i) ; $(gstat -c '%a' $i) ;  $(gstat -c '%U' $i) ; $(gstat -c '%G' $i)"
-    #echo "$(realpath $i) ; $(stat -f '%A' $i) ;  $(gstat -c '%U' file1) ; $(gstat -c '%G' file1)"
-done
