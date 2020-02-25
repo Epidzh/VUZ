b87d564bcd546dde3fae7920f8d3505f9b701e55
e1nfalt <sergio.petrovx@yandex.ru>
2020-02-25 22:51:43 +0300

    gauss complete

--- py/maths/gauss.py
+++ py/maths/gauss.py
@@ -1,14 +1,55 @@
-def first(arr):
-
-
-def second():
-
-
 koeffs = [
     [1, 4, -9, 7, -67],
-    [2, -2, -2, 3. -57],
+    [2, -2, -2, 3, -57],
     [-1, 3, -9, -1, -26],
     [-5, 2, 2, 1, 52]
 ]
-koeffs = first(koeffs)
+
+def draw(arr):
+    print("-------------------------------------")
+    for i in arr:
+        print(i)
+    print("------------------------------------\n")
+
+
+def first(arr):
+    steps = len(arr) - 1
+    main_elem = arr[0][0]
+    for iteration in range(steps):
+        if main_elem == 0:
+            for j in range(len(arr)):
+                if arr[j][iteration] != 0:
+                    arr[iteration], arr[j] = arr[j], arr[iteration]
+                    break
+            
+        main_elem = arr[iteration][iteration]
+        for j in range(iteration + 1, len(arr)):
+            dev = (-arr[j][iteration] / main_elem)
+            for k in range(iteration, len(arr[j])):
+                arr[j][k] += (arr[iteration][k] * dev)
+        draw(arr)
+    return arr
+
+
+def second(matrix):
+    ans = {i:1 for i in range(len(matrix) + 1)}
+    ans[len(matrix)] = matrix[-1][-1] / matrix[-1][-2]
+    t = 0
+    for i in reversed(matrix[:-1]):
+        sm = 0
+        for k in range(len(i) - 2 - t, len(i) - 1):
+            sm += i[k] * ans[k + 1]
+        ans[len(matrix) - 1 - t] = (i[-1] - sm) / i [len(i) - 2 - t - 1]
+        t += 1
+    return ans
+
+matrix = first(koeffs)
+draw(matrix)
 ans = second(koeffs)
+ans.pop(0)
+for i, j in ans.items():
+    print("x{} = {}".format(i, j))
+# sm = 0
+# for k in range(len(koeffs[0]) - 1):
+#     sm += koeffs[0][k]*ans[k+1]
+# print(sm)
--- py/matstat/first.py
+++ py/matstat/first.py
@@ -14,19 +14,58 @@ def get_random_poisson(l, size=1):
     return poisson(l, size)
 
 
-def vib_sr(arr, N):
-    mx = max(arr)
-    digits = [0 for i in range(mx + 1)]
-    w = [0 for i in range(mx + 1)]
+def get_sr(w, N):
     ans = 0
-    for i in range(mx + 1):
-        for j in arr:
-            digits[i] = digits[i] + 1 if i == j else digits[i]
-        w[i] = digits[i] / N
-        ans += w[i] * i
+    for i in range(len(w)):
+        ans += i*w[i]
     return ans
 
-N=200
+
+def get_disp(w, x):
+    ans = 0
+    for i in range(len(w)):
+        ans += (i - x)*(i - x)*w[i]
+    return ans
+
+
+def get_ni_w(arr, N):
+    mx = max(arr)
+    ni = [0 for i in range(mx + 1)]
+    w = [0 for i in range(len(ni))]
+    for i in range(mx + 1):
+        for j in arr:
+            ni[i] = ni[i] + 1 if i == j else ni[i]
+        w[i] = ni[i] / N
+    return [ni, w]
+
+
+def get_func(w, x):
+    if (x < w[0]):
+        return 0
+    elif x > w[-1]:
+        return 1
+    else:
+        ans = 0
+        i = 0
+        while w[i] < x:
+            ans += w[i]
+            i += 1
+        return ans
+
+
+def get_med(w):
+    ans = 0
+    for i in range(1, len(w) - 1):
+        if get_func(w, i - 1) < 0.5 and get_func(w, i) > 0.5:
+            ans = i
+            break
+        elif get_func(w, x) == 0.5:
+            ans = 0.5*(i + i + 1)
+            break
+    return ans
+
+
+N = 200
 v = 54
 n = 5 + (v % 16)
 p = 0.3 + 0.005 * v
@@ -34,24 +73,45 @@ mo = n*p
 D = n*p*(1 - p)
 G = sqrt(D)
 moda = [(n + 1)*p]
-medi = round(n*p)
 ass = ((1 - p) - p ) / G
 eks = (1 - n*p ) / D
+print("n={} ; p={}".format(n , p))
 
-binom = get_random_binomial(n, p, N)
 print("Binom")
+binom = get_random_binomial(n, p, N)
 print(binom)
 print(sorted(binom))
-print("Выборочное среднее: ", vib_sr(binom, N))
+ni, w = get_ni_w(binom, N)
+x = get_sr(w, N)
+disp = get_disp(w, x)
+print("Выборочное среднее: ", x)
+print("Выборочная дисперсия: ", disp)
+print("Выборочное среднее квадратическое отклонение: ", sqrt(disp))
+print("Выборочная мода:", ni.index(max(ni)))
+print("Выборочная медиана:", get_med(w))
 
-geom = get_random_geometric(p, N)
 print("Geom")
+geom = get_random_geometric(p, N)
 print(geom)
 print(sorted(geom))
-print("Выборочное среднее: ", vib_sr(geom, N))
+ni, w = get_ni_w(geom, N)
+x = get_sr(w, N)
+disp = get_disp(w, x)
+print("Выборочное среднее: ", x)
+print("Выборочная дисперсия: ", disp)
+print("Выборочное среднее квадратическое отклонение: ", sqrt(disp))
+print("Выборочная мода:", ni.index(max(ni)))
+print("Выборочная медиана:", get_med(w))
 
-poisson = get_random_poisson(n*p, N)
 print("Poisson")
+poisson = get_random_poisson(n*p, N)
 print(poisson)
 print(sorted(poisson))
-print("Выборочное среднее: ", vib_sr(poisson, N))
+ni, w = get_ni_w(poisson, N)
+x = get_sr(w, N)
+disp = get_disp(w, x)
+print("Выборочное среднее: ", x)
+print("Выборочная дисперсия: ", disp)
+print("Выборочное среднее квадратическое отклонение: ", sqrt(disp))
+print("Выборочная мода:", ni.index(max(ni)))
+print("Выборочная медиана:", get_med(w))
