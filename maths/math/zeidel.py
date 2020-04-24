c90964e1a7a37f7eb475e1bd6790c784d5b0e3ac
seryogasx <sergio.petrovx@yandex.ru>
2020-04-24 20:53:35 +0300

    final zeidel

--- maths/math/zeidel.py
+++ maths/math/zeidel.py
@@ -10,6 +10,63 @@ def check(tmp, ans, eps):
     else:
         return True
 
+
+def zeidel(slau):
+    ans = [slau[0][0] if i == 0 else 0 for i in range(len(slau))]
+    # print(ans)
+    prev = [0 for i in range(N)]
+    while check(prev, ans, eps):
+        prev = ans.copy()
+        for i in range(len(slau)):
+            rez = slau[i][0]
+            prev_index = 0
+            for j in slau[i][1:]:
+                if prev_index == i:
+                    prev_index += 1
+                rez += j * prev[prev_index]
+                prev_index += 1
+            ans[i] = rez
+    return ans
+
+
+def simple(slau, a, b):
+    alpha = [[-a[i][j]/a[i][i] if i != j else 0 for j in range(len(a[i]))] for i in range(len(a))]
+    print(alpha)
+    beta = [b[i] / a[i][i] for i in range(len(b))]
+    print(beta)
+    ans = [0 for i in beta]
+    prev = []
+    while check(prev, ans, eps):
+        prev = ans.copy()
+        for i in range(len(alpha)):
+            sm = 0
+            for j in range(len(prev)):
+                sm += alpha[i][j] * prev[j]
+            sm += beta[i]
+            ans[i] = sm
+    return ans
+
+
+def print_ans(ans, a):
+    print("Ответы: ")
+    for i in enumerate(ans):
+        print("x{} = {}".format(i[0]+1, i[1]))
+
+    print("\nПодстановка:")
+    for i in enumerate(a):
+        print("{}-ое уравнение ->".format(i[0]+1), end=' ')
+        sm = 0
+        for j in enumerate(i[1]):
+            sm += j[1] * ans[j[0]]
+            if j[0] == 0:
+                print("{} * {}".format(j[1], ans[j[0]]), end=' ')
+            else:
+                if j[1] * ans[j[0]] < 0:
+                    print(" - {} * {}".format(fabs(j[1]), fabs(ans[j[0]])), end=' ')
+                else:
+                    print(" + {} * {}".format(fabs(j[1]), fabs(ans[j[0]])), end=' ')
+        print("= ", sm)
+
 N = 4
 eps = 0.01
 
@@ -28,37 +85,11 @@ for i in range(len(a)):
     slau[-1].insert(0, b[i] / a[i][i])
 # print(slau)
 
-ans = [slau[0][0] if i == 0 else 0 for i in range(len(slau))]
-# print(ans)
-prev = [0 for i in range(N)]
-while check(prev, ans, eps):
-    prev = ans.copy()
-    for i in range(len(slau)):
-        rez = slau[i][0]
-        prev_index = 0
-        for j in slau[i][1:]:
-            if prev_index == i:
-                prev_index += 1
-            rez += j * prev[prev_index]
-            prev_index += 1
-        ans[i] = rez
+ans = simple(slau, a, b)
+print("Метод простой итерации: ")
+print_ans(ans, a)
 
-print("Ответы: ")
-for i in enumerate(ans):
-    print("x{} = {}".format(i[0]+1, i[1]))
-
-print("\nПодстановка:")
-for i in enumerate(a):
-    print("{}-ое уравнение ->".format(i[0]+1), end=' ')
-    sm = 0
-    for j in enumerate(i[1]):
-        sm += j[1] * ans[j[0]]
-        if j[0] == 0:
-            print("{} * {}".format(j[1], ans[j[0]]), end=' ')
-        else:
-            if j[1] * ans[j[0]] < 0:
-                print(" - {} * {}".format(fabs(j[1]), fabs(ans[j[0]])), end=' ')
-            else:
-                print(" + {} * {}".format(fabs(j[1]), fabs(ans[j[0]])), end=' ')
-    print("= ", sm)
+ans = zeidel(slau)
+print("\nМетод Зейделя: ")
+print_ans(ans, a)
 print('Эталонные значения: ', b)
