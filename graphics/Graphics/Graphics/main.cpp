8eda997e1d9806a3e45d56f74eeb83dab9884d53
seryogasx <sergio.petrovx@yandex.ru>
2020-11-10 17:25:47 +0300

    add first graphics lab

--- graphics/Graphics/Graphics/main.cpp
+++ graphics/Graphics/Graphics/main.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #ifdef __APPLE__
     #define GL_SILENCE_DEPRECATION
     #include <OpenGL/gl.h>
@@ -12,16 +10,12 @@
 
 void init()
 {
-    glClearColor(1.0, 1.0, 1.0, 0.0);
-    glEnable(GL_DEPTH_TEST);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
     glEnable(GL_LIGHTING);
-    glDepthFunc(GL_LEQUAL);
     glEnable(GL_LIGHT0);
-    GLfloat light_position[] = {10.0f, 0.0f, 0.0f};
-    GLfloat spot_direction[] = {0.0f, 0.0f, 0.0f};
-//
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-    glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spot_direction);
+    GLfloat light_direction[] = {1.0f, 1.0f, 1.0f};
+    glLightfv(GL_LIGHT0, GL_POSITION, light_direction);
     glShadeModel(GL_SMOOTH);
 }
 
@@ -29,23 +23,21 @@ void display()
 {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(130, 1, 50, 0);
-//    glMatrixMode(GL_MODELVIEW);
-//    glLoadIdentity();
-//    glOrtho(0, 640, 0, 480, -10, 10);
-//    glMatrixMode(GL_MODELVIEW);
-//    glLoadIdentity();
-//    glLoadIdentity();
+    gluPerspective(130, 1, 0.1, 1000);
     
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(20, 20, 20, 0, 0, 0, 0, 1, 0);
+
     GLdouble points[][3] = {
-        {3.0, 0.0, 0.0},
-        {0.0, 3.0, 0.0},
+        {3.0, 0.0, 0},
+        {0.0, 3.0, 0},
         {-3.0, 0.0, 0},
         {-1.0, -3.0, 0},
         {1.0, -3.0, 0}
     };
-    
-    GLdouble colors[][3] = {
+
+    GLfloat colors[][3] = {
         {1, 0, 0},
         {0, 1, 0},
         {0, 0, 1},
@@ -53,59 +45,41 @@ void display()
         {1, 0, 1}
     };
     
-    GLfloat material_diffuse[] = {1.0, 1.0, 1.0, 1.0};
-    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_diffuse);
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-    glBegin(GL_TRIANGLES);
     
-//    for (int i = 0; i < 5; i++){
-//        glColor3d(colors[i][0], colors[i][1], colors[i][2]);
-//        glVertex3d(0.0, 0.0, 0.0);
-//        glVertex3d(points[i][0], points[i][1], points[i][2]);
-//        glVertex3d(points[(i + 1) % 5][0], points[(i + 1) % 5][1], points[(i + 1) % 5][2]);
-//    }
-    glColor3d(1, 0, 0);
-    glVertex3d(0.0, 0.0, 0.0);
-    glVertex3d(0.0, 1.0, 0.0);
-    glVertex3d(1.0, 0.0, 0.0);
-
-    glColor3d(0, 1, 0);
-    glVertex3d(0.0, 0.0, 0);
-    glVertex3d(0.0, 1.0, 0);
-    glVertex3d(-1.0, 0.0, 0);
-
-    glColor3d(0, 0, 1);
-    glVertex3d(0.0, 0.0, 0);
-    glVertex3d(-0.333, -1.0, 0);
-    glVertex3d(0.333, -1.0, 0);
-
-    glColor3d(1, 1, 0);
-    glVertex3d(0.0, 0.0, 0);
-    glVertex3d(-1.0, 0.0, 0);
-    glVertex3d(-0.333, -1.0, 0);
-
-    glColor3d(1, 0, 1);
-    glVertex3d(0.0, 0.0, 0);
-    glVertex3d(0.333, -1.0, 0);
-    glVertex3d(1.0, 0.0, 0);
-
+    glBegin(GL_TRIANGLES);
+    for (int i = 0; i < 5; i++){
+        GLfloat material_diffuse[] = {colors[i][0], colors[i][1], colors[i][2], 1.0};
+        glColorMaterial(GL_FRONT_AND_BACK, GL_DIFFUSE);
+        glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, material_diffuse);
+        glColor3d(colors[i][0], colors[i][1], colors[i][2]);
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(points[i][0], points[i][1], points[i][2]);
+        glVertex3f(points[(i + 1) % 5][0], points[(i + 1) % 5][1], points[(i + 1) % 5][2]);
+    }
     glEnd();
-
+    
     glutSwapBuffers();
 }
 
-int main(int argc, char * argv[]) {
+void reshape(int width, int height) {
+    if (height == 0) height = 1;
+    float ratio = 1.0* width / height;
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glViewport(0, 0, width, height);
+    gluPerspective(130, ratio, 0.1, 1000);
+    glMatrixMode(GL_MODELVIEW);
+}
 
+int main(int argc, char **argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGBA);
-    glutInitWindowSize(512, 512);
-    glutInitWindowPosition(0, 0);
-    glutCreateWindow("First Lab");
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(400,400);
+    glutCreateWindow("Задача 1");
     init();
     glutDisplayFunc(display);
-//    glutReshapeFunc()
-//    glutFullScreen();
+    glutReshapeFunc(reshape);
     glutMainLoop();
-    return 0;
 }
